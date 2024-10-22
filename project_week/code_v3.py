a6553b2b6f0795012316d06903944c7d710838ed
BAPTISTE Félix <fbaptiste@myreport.fr>
2024-10-22 14:22:49 +0200

    commit test

--- project_week/code_v3.py
+++ project_week/code_v3.py
@@ -6,23 +6,31 @@ pygame.init()
 
 clock = pygame.time.Clock()
 pygame.display.set_caption("This is not a GAME")
-ecran = pygame.display.set_mode((600, 300))
+ecran = pygame.display.set_mode((800, 400))
 background = pygame.image.load('fond.jpg')
-bg = pygame.transform.scale(background, (600, 300))
+bg = pygame.transform.scale(background, (800, 400))
 continuer = True
 
 class Player(pygame.sprite.Sprite):
+    player = pygame.image.load('tank_sprite.jpg')
+    player.set_colorkey((213, 213, 213))
+    pl = pygame.transform.scale(player, (60, 60))
+
     def __init__(self, x, y, velocity_x, velocity_y):
         super().__init__()
         self.position = [x, y]
         self.velocity = [velocity_x, velocity_y]
+        self.image = Player.pl
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.position
 
     def update(self):
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
+        self.rect.topleft = self.position
 
     def draw(self, surface):
-        pygame.draw.circle(surface, (255, 0, 0), self.position, 10)
+       surface.blit(self.image, self.rect.topleft)
 
     def move_left(self):
         self.velocity[0] = -1
